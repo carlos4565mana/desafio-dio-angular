44b99c5068229b88f837ef94a08dcb769501aed5
carlos4565mana <carloscal61@gmail.com>
2022-08-03 17:11:40 -0300

    Create the Cartservice

---
+++ src/app/service/cart.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  public cartItemList : any = []
+  public productList = new BehaviorSubject<any>([]);
+
+  constructor() {}
+    getProducts(){
+      return this.productList.asObservable();
+
+    }
+    setProduct(product : any){
+      this.cartItemList.push(...product);
+      this.productList.next(product);
+    }
+
+    addtoCart(product : any){
+      this.cartItemList.push(product);
+      this.productList.next(this.cartItemList);
+      this.getTotalPrice();
+    }
+
+    getTotalPrice(){
+      let grandTotal = 0;
+      this.cartItemList.map((a:any)=>{
+        grandTotal =+ a.total;
+      })
+    }
+   
+}
